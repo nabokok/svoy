939cd6c0800e403be12ff4cfb08c50b0c8bfba11
Alyona Nabok <alyonana42@gmail.com>
2024-04-19 00:23:13 +0300

    add magnetic button

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import Button from '../ui/button/button';
 import GradientButton from '../ui/gradientButton/gradientButton';
 import styles from './hero.module.css';
+
 
 function Hero() {
   return (
--- app/components/ui/circleButton/circleButton.tsx
+++ app/components/ui/circleButton/circleButton.tsx
@@ -1,16 +1,62 @@
-import { ReactNode } from 'react';
-import styles from './circleButton.module.css'
+'use client'
+
+import React, { ReactNode, useRef } from 'react';
+import { gsap } from 'gsap';
+import styles from './circleButton.module.css';
 
 interface Props {
   children: ReactNode;
 }
 
 function CircleButton({ children }: Props) {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const textRef = useRef<HTMLDivElement | null>(null);
+
+  const updateButtonTransform = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const button = buttonRef.current;
+    const text = textRef.current;
+
+    if (button !== null && text !== null) {
+      const rect = button.getBoundingClientRect();
+      const mouseX = event.clientX - rect.left - rect.width / 2;
+      const mouseY = event.clientY - rect.top - rect.height / 2;
+
+
+      gsap.to([button, text], {
+        x: mouseX * 0.3,
+        y: mouseY * 0.3,
+        duration: 0.3,
+        ease: 'power1.out',
+      });
+    }
+  };
+
+  const resetButtonTransform = () => {
+    const button = buttonRef.current;
+    const text = textRef.current;
+
+    if (button !== null && text !== null) {
+      gsap.to([button, text], {
+        x: 0,
+        y: 0,
+        duration: 0.3,
+        ease: 'power1.out',
+      });
+    }
+  };
+
   return (
-    <button className={styles.circleButton}>
-      {children}
+    <button
+      ref={buttonRef}
+      className={styles.circleButton}
+      onMouseMove={updateButtonTransform}
+      onMouseLeave={resetButtonTransform}
+    >
+      <div ref={textRef} className={styles.textWrapper}>
+        {children}
+      </div>
     </button>
-  )
+  );
 }
 
 export default CircleButton;
